89b400edb52e2bc46688152f9e35071c357f48af
Higor Borjaille <higorborjaille@pextra.co>
2016-09-11 20:04:25 -0300

    Changed the package name with others bug fixing (more details on issue #4) closes #4

--- scene-core/src/main/java/br/ufes/inf/lprm/scene/base/SituationHelper.java
+++ scene-core/src/main/java/br/ufes/inf/lprm/scene/base/SituationHelper.java
@@ -1,10 +1,9 @@
 package br.ufes.inf.lprm.scene.base;
 
-import br.ufes.inf.lprm.scene.publishing.SituationPublisher;
-import br.ufes.inf.lprm.scene.spm.CastRestoreType;
-import br.ufes.inf.lprm.scene.spm.SituationProfile;
-import br.ufes.inf.lprm.scene.spm.SituationProfileManager;
-import br.ufes.inf.lprm.situation.*;
+import br.ufes.inf.lprm.situation.Role;
+import br.ufes.inf.lprm.situation.SituationCast;
+import br.ufes.inf.lprm.situation.SituationType;
+import br.ufes.inf.lprm.situation.SituationUtils;
 import br.ufes.inf.lprm.situation.events.ActivateSituationEvent;
 import br.ufes.inf.lprm.situation.events.DeactivateSituationEvent;
 import org.drools.core.base.SalienceInteger;
@@ -13,10 +12,8 @@ import org.drools.core.spi.KnowledgeHelper;
 import org.kie.api.KieBase;
 import org.kie.api.definition.KiePackage;
 
-import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 public class SituationHelper {
 	
@@ -87,8 +84,6 @@ public class SituationHelper {
 	}
 	
 	public static SituationType activateSituation(KnowledgeHelper khelper, SituationCast cast, Class<?> type, long timestamp) {
-
-		SituationProfile prof = SituationProfileManager.getInstance().getProfile(type.getName());		
 		
 		long evn_timestamp = khelper.getKieRuntime().getSessionClock().getCurrentTime();
 		ActivateSituationEvent ase = new ActivateSituationEvent(evn_timestamp);
@@ -99,18 +94,10 @@ public class SituationHelper {
 			
 			sit = (SituationType) type.newInstance();
 			
-			if (prof.getSnapshot()) {
-				sit.getSnapshots().add(new CastSnapshot(cast, evn_timestamp));
-			}
-			
 			SetFieldsFromMatchedObjects(sit, cast);
 			ase.setSituation(sit);
 			
-			sit.setActivation(ase);	
-			
-			SituationPublisher pub = SituationProfileManager.getInstance().getConfigurationHash().get(type.getName()).getPublisher();
-			
-			if (pub!=null) pub.publishActivation(sit);
+			sit.setActivation(ase);
 
 			khelper.getKieRuntime().insert(ase);
 			khelper.getKieRuntime().insert(sit);
@@ -118,8 +105,6 @@ public class SituationHelper {
 		} catch (InstantiationException e) {
 			e.printStackTrace();
 		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
@@ -131,30 +116,7 @@ public class SituationHelper {
 		DeactivateSituationEvent dse = new DeactivateSituationEvent(evn_timestamp);
 		dse.setSituation((SituationType) sit);
 
-		SituationProfile prof = SituationProfileManager.getInstance().getProfile(sit.getClass().getName());
-			
-		if (prof.getSnapshot()) {
-			try {
-				CastSnapshot snap = null;
-				try {
-					if (prof.getRestoretype() == CastRestoreType.FIRST) snap 	= ((SituationType) sit).getSnapshots().getFirst(); 
-					if (prof.getRestoretype() == CastRestoreType.STABLE) snap 	= ((SituationType) sit).getSnapshots().getStable(); 
-					if (prof.getRestoretype() == CastRestoreType.LAST) snap 	= ((SituationType) sit).getSnapshots().getLast();
-				} catch(NoSuchElementException nsee) {
-					snap = null;
-				}
-				if (snap != null) SetFieldsFromMatchedObjects(sit, snap.getSituationCast(SituationProfileManager.getInstance().getClassLoader()));
-			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			}			
-		}
 		((SituationType) sit).setDeactivation(dse);
-		
-		SituationPublisher pub = SituationProfileManager.getInstance().getConfigurationHash().get(sit.getClass().getName()).getPublisher();
-		
-		if (pub!=null) pub.publishDeactivation((SituationType) sit);		
 		
 		khelper.getKieRuntime().insert(dse);
 		khelper.getKieRuntime().update(khelper.getKieRuntime().getFactHandle(sit), sit);
