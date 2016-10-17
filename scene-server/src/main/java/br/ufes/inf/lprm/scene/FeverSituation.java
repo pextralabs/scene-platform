abaa05505ebd3877937c7d54c4eee09368dd957f
Higor Borjaille <hborjaille@pextra.co>
2016-10-17 14:02:40 -0200

    JSON mock modified

--- scene-server/src/main/java/br/ufes/inf/lprm/scene/FeverSituation.java
+++ scene-server/src/main/java/br/ufes/inf/lprm/scene/FeverSituation.java
@@ -15,35 +15,75 @@ public class FeverSituation {
             file  = new File("/Users/hborjaille/Projects/scene-platform/scene-server/src/main/mock/insertfeverdata.json");
             code.insertData(file);
 
-            Thread.sleep(3000);
+            Thread.sleep(1000);
 
             file  = new File("/Users/hborjaille/Projects/scene-platform/scene-server/src/main/mock/updatefeverdata1.json");
             code.updateData(file);
 
-            Thread.sleep(3000);
+            Thread.sleep(1000);
 
             file  = new File("/Users/hborjaille/Projects/scene-platform/scene-server/src/main/mock/updatefeverdata2.json");
             code.updateData(file);
 
-            Thread.sleep(3000);
+            Thread.sleep(1000);
 
             file  = new File("/Users/hborjaille/Projects/scene-platform/scene-server/src/main/mock/updatefeverdata3.json");
             code.updateData(file);
 
-            Thread.sleep(3000);
+            Thread.sleep(1000);
 
             file  = new File("/Users/hborjaille/Projects/scene-platform/scene-server/src/main/mock/updatefeverdata2.json");
             code.updateData(file);
 
-            Thread.sleep(3000);
+            Thread.sleep(1000);
 
             file  = new File("/Users/hborjaille/Projects/scene-platform/scene-server/src/main/mock/updatefeverdata1.json");
             code.updateData(file);
 
-            Thread.sleep(3000);
+            Thread.sleep(1000);
 
             file  = new File("/Users/hborjaille/Projects/scene-platform/scene-server/src/main/mock/updatefeverdata4.json");
             code.updateData(file);
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+
+        file = new File("/Users/hborjaille/Projects/scene-platform/scene-server/src/main/mock/insertfeverapp2.json");
+        CodeInsert code2 = new CodeInsert(file);
+
+        try {
+            file  = new File("/Users/hborjaille/Projects/scene-platform/scene-server/src/main/mock/insertfeverdata.json");
+            code2.insertData(file);
+
+            Thread.sleep(1000);
+
+            file  = new File("/Users/hborjaille/Projects/scene-platform/scene-server/src/main/mock/updatefeverdata1.json");
+            code2.updateData(file);
+
+            Thread.sleep(1000);
+
+            file  = new File("/Users/hborjaille/Projects/scene-platform/scene-server/src/main/mock/updatefeverdata2.json");
+            code2.updateData(file);
+
+            Thread.sleep(1000);
+
+            file  = new File("/Users/hborjaille/Projects/scene-platform/scene-server/src/main/mock/updatefeverdata3.json");
+            code2.updateData(file);
+
+            Thread.sleep(1000);
+
+            file  = new File("/Users/hborjaille/Projects/scene-platform/scene-server/src/main/mock/updatefeverdata2.json");
+            code2.updateData(file);
+
+            Thread.sleep(1000);
+
+            file  = new File("/Users/hborjaille/Projects/scene-platform/scene-server/src/main/mock/updatefeverdata1.json");
+            code2.updateData(file);
+
+            Thread.sleep(1000);
+
+            file  = new File("/Users/hborjaille/Projects/scene-platform/scene-server/src/main/mock/updatefeverdata4.json");
+            code2.updateData(file);
         } catch (Throwable t) {
             t.printStackTrace();
         }
